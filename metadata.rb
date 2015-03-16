5c1d9eb81b6828ac51aefc414a3c02e7c68d753b
Bryan Karaffa <bryankaraffa@gmail.com>
2015-03-16 14:18:35 -0700

    Added some RightScale specific metadata to metadata.rb

--- metadata.rb
+++ metadata.rb
@@ -1,2 +1,10 @@
 name "bk-remote_recipe"
 version "0.1.0"
+
+supports "centos"
+supports "ubuntu" 
+supports "redhat"
+
+depends "rightscale"
+
+recipe "bk-remote_recipe::default", "My default cookbook::recipe"
